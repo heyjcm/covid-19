a0ea2a20969a8753cc0e78ef6022a81dd8dfc269
Christian Montecillo <afchristian@gmail.com>
2020-04-23 09:24:28 -0600

    Deaths per million graphs created

--- CV19.R
+++ CV19.R
@@ -13,18 +13,26 @@ library(scales)
 # US numbers
 deaths_us <- read.csv(text = getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv"))
 confirmed_us <- read.csv(text = getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"))
-recovered_us <- read.csv(text = getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/04-21-2020.csv"))
+
+# US numbers local (uncomment this section in case GitHub isn't working, hopefully this local data is up to date!!!)
+# deaths_us <- read.csv("JHU Repo/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv")
+# confirmed_us <- read.csv("JHU Repo/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
 
 # world numbers
 world_deaths <- read.csv(text = getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"))
 world_recovered <- read.csv(text = getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"))
 world_confirmed <- read.csv(text = getURL("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"))
+
+# world numbers local (uncomment this section in case GitHub isn't working, hopefully this local data is up to date!!!)
+# world_deaths <- read.csv("JHU Repo/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv")
+# world_recovered <- read.csv("JHU Repo/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv")
+# world_confirmed <- read.csv("JHU Repo/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+
 #### end load csv ####
 
-
 # columns that will not be used from full DFs deaths_us and confirmed_us
-positions_to_remove <- c(1:6, 8:81)
-
+positions_to_remove <- c(1:6, 8:11, 13:81)
+#positions_to_remove <- c(1:6, 8:81)
 
 #### deaths by state ####
 states_death <- deaths_us %>%
@@ -35,17 +43,40 @@ states_death <- deaths_us %>%
            Province_State == "Wisconsin" |
            Province_State == "Oklahoma" |
            Province_State == "Kentucky" |
-           Province_State == "North Carolina") %>% 
+           Province_State == "North Carolina" |
+           Province_State == "Georgia") %>%
   select(-positions_to_remove)
 
+# DF for state populations based on states_death filter
+state_pop <- deaths_us %>% 
+  group_by(Province_State) %>% 
+  summarize(Population = sum(Population / 1000000))
+
 # massage data
-date_columns <- colnames(states_death[, 2:ncol(states_death)]) # date_columns to be used in melt function below
+date_columns <- colnames(states_death[, 3:ncol(states_death)]) # date_columns to be used in melt function below
 death_data <- melt(states_death, id.vars = "Province_State", measure.vars = date_columns)
-death_data$variable<- str_replace(death_data$variable, "X", "") # just removing the 'X' from the column
+death_data$variable <- str_replace(death_data$variable, "X", "") # just removing the 'X' from the column
 death_data$variable <- mdy(death_data$variable) # change to month/day/year format
 death_data <- rename(death_data, c(variable = "date", value = "deaths")) # rename column names for use later
 
-# create death_data DF with State, date, and sum of deaths per State by date
+# merge the state_pop table with the death_data table
+death_data$Population <- state_pop$Population[match(death_data$Province_State, state_pop$Province_State)]
+
+# create death_data DF with State, date, and sum of deaths per State by date to be used for population-weighted graph
+deaths_per_million_data <- death_data %>%
+  group_by(Province_State, date) %>%
+  summarize(total_deaths = sum(deaths / Population))
+
+# calculate deaths per million
+deaths_per_million_aggregated <- death_data %>%
+  group_by(Province_State, date) %>%
+  summarize(total_deaths = sum(deaths / Population)) %>% 
+  filter(date == Sys.Date() - 1) # -1 day because today's data may not be populated yet
+
+# write the daily deaths/million (death_data_abbi_today) table to a .csv
+write.csv(deaths_per_million_aggregated, paste("Tables/", Sys.Date(), ".csv", sep = ""))
+
+# create death_data DF with State, date, and sum of deaths per State by date to be used for log and linear plots
 death_data <- death_data %>%
   group_by(Province_State, date) %>%
   summarize(total_deaths = sum(deaths))
@@ -72,7 +103,7 @@ death_plot_log
 ### end plot deaths, logarithmic ###
 
 ### plot deaths, linear ###
-death_plot_reg <- death_data %>% 
+death_plot_linear <- death_data %>% 
   ggplot(aes(x = date, y = total_deaths, color = Province_State, group = Province_State)) +
   ggtitle("COVID-19 Deaths, [Linear Scale]") +
   theme(plot.title = element_text(hjust = 0.5)) +
@@ -88,8 +119,50 @@ death_plot_reg <- death_data %>%
   annotate("text", x = as.Date("2020-04-04"), y = 1000, label = "= Texas, 20 Apr 2020\n[507 Deaths]", color = "Purple", size = 5, hjust = 0) +
   annotate("text", x = as.Date("2020-04-03"), y = 990, label = "*", color = "Purple", size = 20)
 
-death_plot_reg
-### end plot deaths, linear ###
+death_plot_linear
+### end deaths plot, linear ###
+
+### plot deaths per million, linear ###
+death_per_million_plot <- deaths_per_million_data %>% 
+  ggplot(aes(x = date, y = total_deaths, color = Province_State, group = Province_State)) +
+  ggtitle("COVID-19 Deaths per Million, [Linear Scale]") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_point(show.legend = TRUE) +
+  geom_smooth() +
+  ylab("Deaths per Million") + # name the y-axis
+  xlab("Date") + # name the x-axis
+  scale_color_discrete("State") + # name the legend
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_date(date_labels = "%b %d", date_breaks = "1 day", minor_breaks = NULL) +
+  geom_vline(xintercept = as.numeric(as.Date("2020-04-20")), linetype=3)# +
+  # annotate("text", x = as.Date("2020-04-20"), y = 470, label = "*", color = "Purple", size = 20) +
+  # annotate("text", x = as.Date("2020-04-04"), y = 1000, label = "= Texas, 20 Apr 2020\n[507 Deaths]", color = "Purple", size = 5, hjust = 0) +
+  # annotate("text", x = as.Date("2020-04-03"), y = 990, label = "*", color = "Purple", size = 20)
+
+death_per_million_plot
+### end deaths per million plot ###
+
+### plot deaths per million, linear ###
+death_per_million_plot_log <- deaths_per_million_data %>% 
+  ggplot(aes(x = date, y = total_deaths, color = Province_State, group = Province_State)) +
+  ggtitle("COVID-19 Deaths per Million, [Log Scale]") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_point(show.legend = TRUE) +
+  geom_smooth() +
+  ylab("Deaths per Million") + # name the y-axis
+  xlab("Date") + # name the x-axis
+  scale_color_discrete("State") + # name the legend
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  scale_x_date(date_labels = "%b %d", date_breaks = "1 day", minor_breaks = NULL) +
+  scale_y_log10() +
+  geom_vline(xintercept = as.numeric(as.Date("2020-04-20")), linetype=3)# +
+# annotate("text", x = as.Date("2020-04-20"), y = 470, label = "*", color = "Purple", size = 20) +
+# annotate("text", x = as.Date("2020-04-04"), y = 1000, label = "= Texas, 20 Apr 2020\n[507 Deaths]", color = "Purple", size = 5, hjust = 0) +
+# annotate("text", x = as.Date("2020-04-03"), y = 990, label = "*", color = "Purple", size = 20)
+
+death_per_million_plot_log
+### end deaths per million plot ###
+
 
 #### confirmed data by state ####
 # create df called states_confirmed from full df confirmed_us
@@ -103,7 +176,8 @@ states_confirmed <- confirmed_us %>%
            Province_State == "Wisconsin" |
            Province_State == "Oklahoma" |
            Province_State == "Kentucky" |
-           Province_State == "North Carolina") %>% 
+           Province_State == "North Carolina" |
+           Province_State == "Georgia") %>% 
   select(-positions_to_remove) # selecting all but the the positions_to_remove columns
 
 date_columns <- colnames(states_confirmed[, 2:ncol(states_confirmed)])
@@ -121,7 +195,7 @@ confirmed_plot_log <- confirmed_data %>%
   ggtitle("Confirmed COVID-19 Cases, [Logarithmic Scale]") +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_point(show.legend = TRUE) +
-  geom_line() +
+  geom_smooth() +
   ylab("Confirmed Cases") + # name the y-axis
   xlab("Date") + # name the x-axis
   scale_color_discrete("State") + # name the legend
@@ -157,55 +231,65 @@ confirmed_plot_reg
 global_positions_to_remove <- c(1, 3:4) # indeces of the columns that aren't needed in the graph
 
 # create DF of US deaths
-US_deaths <- world_deaths %>%
-  filter(Country.Region == "US") %>% 
+global_deaths <- world_deaths %>%
+  filter(Country.Region == "US" |
+           Country.Region == "Germany" |
+           Country.Region == "Korea, South" |
+           Country.Region == "Italy" |
+           Country.Region == "") %>%
   select(-global_positions_to_remove)
 
 # process to massage data
-date_columns <- colnames(US_deaths[, 2:ncol(US_deaths)]) # grab the column headers (except for first one)
-US_death_data <- melt(US_deaths, id.vars = "Country.Region", measure.vars = date_columns) # melt states per day and by value
-US_death_data$variable<- str_replace(US_death_data$variable, "X", "")
-US_death_data$variable <- mdy(US_death_data$variable)
-US_death_data <- rename(US_death_data, c(variable = "date", value = "US_deaths"))
+date_columns <- colnames(global_deaths[, 2:ncol(global_deaths)]) # grab the column headers (except for first one)
+global_death_data <- melt(global_deaths, id.vars = "Country.Region", measure.vars = date_columns) # melt states per day and by value
+global_death_data$variable<- str_replace(global_death_data$variable, "X", "")
+global_death_data$variable <- mdy(global_death_data$variable)
+global_death_data <- rename(global_death_data, c(variable = "date", value = "global_deaths"))
 
 # create DF of US recovered
-US_recovered <- world_recovered %>%
-  filter(Country.Region == "US") %>% 
+global_recovered <- world_recovered %>%
+  filter(Country.Region == "US" |
+           Country.Region == "Germany" |
+           Country.Region == "Korea, South" |
+           Country.Region == "Italy") %>%
   select(-global_positions_to_remove)
 
-date_columns <- colnames(US_recovered[, 2:ncol(US_recovered)])
-US_recovered_data <- melt(US_recovered, id.vars = "Country.Region", measure.vars = date_columns)
-US_recovered_data$variable<- str_replace(US_recovered_data$variable, "X", "")
-US_recovered_data$variable <- mdy(US_recovered_data$variable)
-US_recovered_data <- rename(US_recovered_data, c(variable = "date", value = "US_recovered"))
+date_columns <- colnames(global_recovered[, 2:ncol(global_recovered)])
+global_recovered_data <- melt(global_recovered, id.vars = "Country.Region", measure.vars = date_columns)
+global_recovered_data$variable<- str_replace(global_recovered_data$variable, "X", "")
+global_recovered_data$variable <- mdy(global_recovered_data$variable)
+global_recovered_data <- rename(global_recovered_data, c(variable = "date", value = "global_recovered"))
 
-
-US_confirmed <- world_confirmed %>%
-  filter(Country.Region == "US") %>% 
+global_confirmed <- world_confirmed %>%
+  filter(Country.Region == "US" |
+           Country.Region == "Germany" |
+           Country.Region == "Korea, South" |
+           Country.Region == "Italy") %>% 
   select(-global_positions_to_remove)
 
-date_columns <- colnames(US_confirmed[, 2:ncol(US_confirmed)])
-US_confirmed_data <- melt(US_confirmed, id.vars = "Country.Region", measure.vars = date_columns)
-US_confirmed_data$variable<- str_replace(US_confirmed_data$variable, "X", "")
-US_confirmed_data$variable <- mdy(US_confirmed_data$variable)
-US_confirmed_data <- rename(US_confirmed_data, c(variable = "date", value = "US_confirmed_data"))
+date_columns <- colnames(global_confirmed[, 2:ncol(global_confirmed)])
+global_confirmed_data <- melt(global_confirmed, id.vars = "Country.Region", measure.vars = date_columns)
+global_confirmed_data$variable<- str_replace(global_confirmed_data$variable, "X", "")
+global_confirmed_data$variable <- mdy(global_confirmed_data$variable)
+global_confirmed_data <- rename(global_confirmed_data, c(variable = "date", value = "global_confirmed_data"))
 
-US_active_df <- US_confirmed_data
-US_active_df$US_deaths <- US_death_data$US_deaths
-US_active_df$US_recovered <- US_recovered_data$US_recovered
-US_active_df <- US_active_df %>%
+global_active_df <- global_confirmed_data
+global_active_df$global_deaths <- global_death_data$global_deaths
+global_active_df$global_recovered <- global_recovered_data$global_recovered
+global_active_df <- global_active_df %>%
   group_by(Country.Region, date) %>% 
-  summarize(US_active = US_confirmed_data - US_deaths - US_recovered)
+  summarize(global_active = global_confirmed_data - global_deaths - global_recovered)
 
-US_plot_reg <- US_active_df %>% 
-  ggplot(aes(x = date, y = US_active, color = Country.Region, group = Country.Region)) +
-  ggtitle("US Active Cases = Confirmed - Deaths - Recovered, [Logarithmic Scale]") +
+global_plot_reg <- global_active_df %>% 
+  ggplot(aes(x = date, y = global_active, color = Country.Region, group = Country.Region)) +
+  #scale_colour_manual(values=cbPalette) +
+  ggtitle("Active Cases = Confirmed - Deaths - Recovered, [Logarithmic Scale]") +
   theme(plot.title = element_text(hjust = 0.5)) +
   geom_point(show.legend = TRUE) +
-  geom_smooth() +
-  ylab("Active US Cases") + # name the y-axis
+  geom_line() +
+  ylab("Active Cases") + # name the y-axis
   xlab("Date") + # name the x-axis
-  scale_color_discrete("US") + # name the legend
+  scale_color_discrete("Country") + # name the legend
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   scale_x_date(date_labels = "%b %d", date_breaks = "3 days", minor_breaks = NULL) +
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x), labels = trans_format("log10", math_format(10^.x))) +
@@ -214,4 +298,4 @@ US_plot_reg <- US_active_df %>%
   annotate("text", x = as.Date("2020-01-27"), y = 30000, label = "= US, 20 Apr 2020\n[669,903 Active Cases]", color = "Purple", size = 5, hjust = 0) +
   annotate("text", x = as.Date("2020-01-24"), y = 29500, label = "*", color = "Purple", size = 18)
 
-US_plot_reg
+global_plot_reg
